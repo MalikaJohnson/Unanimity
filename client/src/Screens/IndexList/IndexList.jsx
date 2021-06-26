872004b18b1ad9d3901520e04fb758a602045de6
Malika Johnson <malikajohnson1995@MacBook-Air.local>
2021-06-25 22:19:39 -0400

    bugs/ conflict in controller

--- client/src/Screens/IndexList/IndexList.jsx
+++ client/src/Screens/IndexList/IndexList.jsx
@@ -5,10 +5,11 @@ export default function IndexList(props) {
   const history = useHistory()
   return (
     <div>
-      <h3>Today I am Grateful For..</h3>
+      
       {lists.map((list) => (
         <div key={list.id} onClick={() => history.push(`/lists/${list.id}`)}>
           <p>{list.user.username}</p>
+          <h3>Today I am Grateful For..</h3>
           <p>{list.input_1}</p>
           <p>{list.input_2}</p>
           <p>{list.input_3}</p>
