4a32745d3ba1c810d0c4beef44e6c7cad248bd1b
Malika Johnson <malikajohnson1995@MacBook-Air.local>
2021-06-26 15:27:17 -0400

    show page comments

--- client/src/Screens/ShowList/ShowList.jsx
+++ client/src/Screens/ShowList/ShowList.jsx
@@ -1,9 +1,22 @@
+import { useHistory, Link } from 'react-router-dom';
 
-export default function ShowList() {
+
+export default function ShowList(props) {
+  const { lists, currentUser, comments } = props
+  const history = useHistory()
   
   return (
     <div>
-      <p>ShowList</p>
+     {comments.map((comment) => (
+        <div className=""key={comment.id} >
+          <p>{comment.content}</p>
+          
+          
+        </div>
+      ))}
+      <Link to= "//lists/:id/edit">
+      <button>Edit List</button>
+      </Link>
     </div>
   ) 
 }
