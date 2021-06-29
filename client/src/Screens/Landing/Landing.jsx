614d557b27c9a3f047a8461fa0d3073f876f7dc2
Malika Johnson <malikajohnson1995@MacBook-Air.local>
2021-06-29 02:24:24 -0400

    added CSS

--- client/src/Screens/Landing/Landing.jsx
+++ client/src/Screens/Landing/Landing.jsx
@@ -8,12 +8,16 @@ export default function Landing() {
   return (
     <div className="landing">
       <img className="landing-img" src="https://i.imgur.com/OMKJKez.png" alt="logo" />
+      <div className="blurb">
       <h3 className="pronounce">Yoo-nuh-ni-muh-tee</h3>
       <p className='landing-content'>noun. the state or quality of being unanimous; a consensus or undivided opinion. Here at Unanimity, our goal is to bring our members closer to experiencing oneness by providing ways
         to practice mindfulness in a supportive community atmosphere</p>
+      </div>
+      <div className='landing-btn'>
       <Link to='/home'>
       <button className='btn-landing'>Namaste</button>
         </Link>
+        </div>
     </div>
   )
 }
