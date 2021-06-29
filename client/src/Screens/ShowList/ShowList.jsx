aa47edc377cd31676ff302e3269c90e738f931e6
Malika Johnson <malikajohnson1995@MacBook-Air.local>
2021-06-28 21:02:15 -0400

    show List CSS

--- client/src/Screens/ShowList/ShowList.jsx
+++ client/src/Screens/ShowList/ShowList.jsx
@@ -2,6 +2,7 @@ import { Link, useParams} from 'react-router-dom';
 import { useEffect, useState } from 'react'
 import { getOneList } from '../../Services/lists';
 import CommentPage from "../../Components/CommentPage";
+import './ShowList.css'
 
 
 export default function ShowList(props) {
@@ -21,13 +22,13 @@ export default function ShowList(props) {
   return (
     <div>
       <div className='detail-list'>
-        <p>Today I am Grateful For ..</p>
-        <p>1. {singleList?.input_1}</p>   
-      <p>2. {singleList?.input_2}</p>
-      <p>3. {singleList?.input_3}</p>   
+        <p className="show-header">Today I am Grateful For ..</p>
+        <p className="show-input">1. {singleList?.input_1}</p>   
+      <p className="show-input">2. {singleList?.input_2}</p>
+      <p className="show-input">3. {singleList?.input_3}</p>   
       </div>
     {singleList?.comments.map((comment) => (
-      <div className="" key={comment.id} >
+      <div className="comment-container" key={comment.id} >
       <div className="comment-bubble">
         <p>{comment.content}</p>
         <p>-{comment.user.username}</p>
@@ -35,10 +36,12 @@ export default function ShowList(props) {
         </div>
         </div>
     ))}
-      <CommentPage id={id} handleCreateComm={handleCreateComm }/>
+      <CommentPage id={id} handleCreateComm={handleCreateComm} />
+      <div className="edit-btn-container">
       <Link to= {`/lists/${id}/edit`}>
-      <button>Edit List</button>
-      </Link>
+      <button className="show-edit-btn">Edit List</button>
+        </Link>
+        </div>
     </div>
   ) 
 }
